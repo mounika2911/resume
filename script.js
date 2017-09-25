0874976d63b8bc5801210f104e2fa3dcf95c5313
mounika2911 <mouni.mani2911@gmail.com>
2017-09-25 10:17:13 +0530

    Inital Comment
    Initial Comment

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#header-text, .header-tag-one').hide();
+    $('#header-text').fadeIn(3000);
+    $('.header-tag-one').show('slide', {direction: 'right'}, 5000);
+    $()
+});
